ce924c52b7bb7f64c24e8d362a0cf7a7eae2361d
Carlos <carlosrafaelortiz95@gmail.com>
2022-11-15 18:55:34 -0500

    messing around

--- day-1/main.cpp
+++ day-1/main.cpp
@@ -3,12 +3,22 @@ using namespace std;
 
 int main()
 {
-    cout << "Please enter a number: ";
+    cout << "Question one: \n"
+            "What is 1 + 1? \n";
 
     int x{ };
     cin >> x;
 
-    cout << "You've entered: " << x << "\n";
+    int question_one_answer { 2 };
+    if (x == question_one_answer) {
+        cout << "Correct, the answer was 2 \n";
+    } else {
+        cout << "Incorrect, the correct answer was: 2 \n" << "you answered: " << x << "\n";
+    }
+
+    //cout << "You've entered: " << x << "\n";
+
     return 0;
 
 }
+
