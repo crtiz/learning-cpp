8f077500df89340c39684999f3437e08b88d92ab
Carlos <carlosrafaelortiz95@gmail.com>
2022-11-15 18:20:02 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Please enter a number: ";
+
+    int x{ };
+    cin >> x;
+
+    cout << "You've entered: " << x << "\n";
+    return 0;
+
+}
