2d0a1d6d8f4fbd70d86ffcda505f5d9c53ddc210
Carlos <carlosrafaelortiz95@gmail.com>
2022-11-16 20:54:25 -0500

    day 2

--- day-2/main.cpp
+++ day-2/main.cpp
@@ -1,16 +1,26 @@
-#include <iostream>
+#include<iostream>
+#include <string>
+#include <algorithm>
 
-int returnFive()
-{
-    return 5;
-}
+using namespace std;
+
 
 int main()
 {
-    std::cout << returnFive() << '\n'; // prints 5
-    std::cout << returnFive() + 2 << '\n'; // prints 7
+    int numbers[3][4] = {
+                        {0,1,2,3},
+                        {4,5,6,7},
+                        {8,9,10,11}
+    };
+    //cout << numbers[2][3];
 
-    returnFive();
+    for (int row = 0; row < 3; row++) {
+        for(int column = 0; column < 4; column++) {
+            cout << numbers[row][column] << "  ";
+        }
+        cout << endl;
+    }
 
+    cout << endl;
     return 0;
 }
